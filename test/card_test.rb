56b1235da898ee65a104de6c743920a5c582474f
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-21 17:01:05 -0700

    Card Code Added

--- test/card_test.rb
+++ test/card_test.rb
@@ -6,7 +6,6 @@ class CardTest < Minitest::Test
 
   def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_instance_of Card, card
   end
 
@@ -24,7 +23,6 @@ class CardTest < Minitest::Test
 
   def test_it_has_a_category
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
     assert_equal :Geography, card.category
   end
 end
