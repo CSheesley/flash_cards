b4c8b4bd685e0de1ef31032f763df2c2a3cfc426
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-22 17:34:59 -0700

    Completed turn.rb file

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+class Turn
+  attr_reader :guess, :card
+
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def correct?
+    @guess == card.answer
+  end
+
+  def feedback
+    if correct? then "Correct!" else "Incorrect." end
+  end
+end
