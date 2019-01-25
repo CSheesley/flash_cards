0a3062388259ffaf208a3d24a967ae12c940ce8f
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-24 18:15:50 -0700

    Syntax changes, clean up

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -16,13 +16,13 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
-  def test_that_it_makes_a_guess #guessck
+  def test_that_it_makes_a_guess
     turn = Turn.new("Juneau", @card)
 
     assert_equal "Juneau", turn.guess
   end
 
-  def test_that_the_guess_is_correct #correct?ck
+  def test_that_the_guess_is_correct
     turn = Turn.new("Juneau", @card)
 
     assert turn.correct?
@@ -34,13 +34,13 @@ class TurnTest < Minitest::Test
     refute turn.correct?
   end
 
-  def test_that_it_receives_correct_feedback #feedback
+  def test_that_it_receives_correct_feedback
   turn = Turn.new("Juneau", @card)
 
     assert_equal "Correct!", turn.feedback
   end
 
-  def test_that_it_receives_incorrect_feedback #feedback
+  def test_that_it_receives_incorrect_feedback
     turn = Turn.new("Nome", @card)
 
     assert_equal "Incorrect.", turn.feedback
