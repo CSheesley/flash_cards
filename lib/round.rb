078a18ca5dfc71745fca3341516e552ef5aca8c9
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-26 14:19:56 -0700

    Take-turn method complete

--- lib/round.rb
+++ lib/round.rb
@@ -1,24 +1,35 @@
+require 'pry'
+
 class Round
-  attr_reader :deck, :turns, :current_card
+  attr_reader :deck, :turns, :current_card, :number_correct, :number_incorrect
 
   def initialize(deck)
     @deck = deck
     @turns = []
     @current_card = @deck.cards[0]
+    @number_correct = 0
+    @number_incorrect = 0
+    @correct_cards = []
   end
 
   def take_turn(guess)
     current_turn = Turn.new(guess, @current_card)
     @turns << current_turn
-    @current_card = @deck.cards[@turns.lengths]
+
+    if current_turn.correct?
+      @number_correct += 1
+      @correct_cards << current_turn
+    else
+      @number_incorrect += 1
+      @correct_cards << current_turn
+    end
+
+    @current_card = @deck.cards[@turns.length]
+    return current_turn
   end
 
-  def feedback
-    if take_turn(guess) == current
 
-  end
 
-  end
 
 
 end
