79fe9aacc34f7f91b3bd09c8ad2a018cf24a78e8
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-27 12:44:23 -0700

    Flashcard_runner file Finalized

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -4,14 +4,18 @@ require './lib/deck'
 require './lib/round'
 require 'pry'
 
-card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+card_1 = Card.new("In which sports city will you find Soldier Field?", "Chicago", :Sports)
+card_2 = Card.new("Which state became the 50th state of the USA in 1959?", "Hawaii", :History)
+card_3 = Card.new("In geometry, what shape does the Pythagorean Theorum refer to?", "Triangle", :STEM)
 card_4 = Card.new("Who holds the all time record for stolen bases in Major League Baseball?", "Rickey Henderson", :Sports)
+card_5 = Card.new("In what state can you find Grand Teton National Park?", "Wyoming", :Geography)
+card_6 = Card.new("Which plane has the most moons?", "Jupiter", :STEM)
+card_7 = Card.new("What is the capital of Spain?", "Madrid", :Geography)
+card_8 = Card.new("In what year did the Civil War begin?", "1861", :History)
 
-cards = [card_1, card_2, card_3, card_4]
-
-deck = Deck.new(cards)
+cards = [card_1, card_2, card_3, card_4, card_5, card_6, card_7, card_8]
+deck = Deck.new(cards.shuffle)
 round = Round.new(deck)
 
 round.start
@@ -28,27 +32,9 @@ end
 puts "****** Game Over! ******"
 puts "You had #{round.number_correct} out of #{round.turns.count} for a total score of #{round.percent_correct}%."
 puts ""
-puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
 puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
+puts "History - #{round.percent_correct_by_category(:History)}% correct"
+puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
 puts "Sports - #{round.percent_correct_by_category(:Sports)}% correct"
 puts ""
-puts "****** Thank You for Playing! ******"
-
-
-
-
-
-
-
-
-
-
-# Start the round using a new method called start (round.start)
-# p current card card question
-# collect answer and store in take_turn argument
-# evaluate and store
-# give feedback
-# next card
-#
-# loop until current_card = @deck.card.count max
-# or to a times loop based on card total?
+puts "   *** Thank You for Playing! ***   "
