35ef7ee77b96d190db315834d05b2bae596f1458
Corey Sheesley <corey.sheesley@gmail.com>
2019-01-28 07:54:00 -0700

    Cards_in_category - now using 'select' method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,11 +6,8 @@ class Deck
   end
 
   def cards_in_category(category_argument)
-    category_cards = []
-    @cards.map do |flash_card| # don't think map is being utilized to full potential here?
-      if flash_card.category == category_argument
-        category_cards << flash_card
-      end
+    category_cards = @cards.select do |flash_card| # don't think map is being utilized to full potential here?
+      flash_card.category == category_argument
     end
     category_cards
   end
